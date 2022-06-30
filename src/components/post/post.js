57ce20a8f50dcc29a559ce16f79442b2fbacc97c
Fares00T <62139647+Fares00T@users.noreply.github.com>
2022-06-30 14:04:51 +0100

    add api links

--- src/components/post/post.js
+++ src/components/post/post.js
@@ -2,7 +2,7 @@ import "./Post.css";
 import { Link } from "react-router-dom";
 
 const Post = ({ post }) => {
-  const PF = "http://localhost:5000/images/";
+  const PF = "https://travel-the-world-blog.herokuapp.com/images/";
   return (
     <div className='post'>
       {post.photo && <img className='postImg' src={PF + post.photo} alt='' />}
